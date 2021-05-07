2f1cff4f5b3f805ef1a6938ec228f6b81bf3a81c
Mauricio Mejía <73669939+MauricioMejS@users.noreply.github.com>
2021-05-06 22:34:47 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+} from "react-native";
+import Loan from "./src/components/Loan/loan";
+import Auth from "./src/components/Auth/Auth";
+import firebase from "./src/utils/firebase";
+import "firebase/auth/";
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((response) => {
+      setUser(response);
+    });
+  }, []);
+
+  if (user === undefined) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.background}>
+        {user ? <Logout /> : <Auth />}
+      </SafeAreaView>
+    </>
+  );
+}
+
+function Logout() {
+  const logout = () => {
+    firebase.auth().signOut();
+  };
+  return (
+    <View >
+      <Loan />
+      <Button
+        style={styles.submitButton}
+        title="cerrar sesión"
+        onPress={logout}
+      ></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#ffffff",
+    height: "100%",
+  },
+  submitButton: {
+    position: "absolute",
+    bottom: 0,
+    
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: "relative",
   },
 });
